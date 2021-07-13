c2846a9dcf86686b26f4b928ae540f5bcbfee245
Matan <email>
2021-07-14 02:58:19 +0300

    Using express module and showing the data only if it is json, nice message when requested unkown path

--- Server.js
+++ Server.js
@@ -1,32 +1,82 @@
-const http = require('http')
+// const http = require('http')
+// const fs = require('fs')
+// const url = require('url')
+
+// const port = 80
+// // var data = fs.readFileSync("DataFile.JSON", "utf8")
+
+// const server = http.createServer(function (request, response) {
+//   fs.readFile("DataFile.JSON", function(err, data){
+//     if (err)
+//     {
+//       console.log("Something went wrong...")
+//       console.log(err)
+//     }
+//     else
+//     {
+//       if (request.url == "/cars")
+//       {
+//         // response.writeHead(200, {"Content-Type": "text/html"})
+//         response.write(data)
+//       }
+//       else
+//       {
+//         response.write("Not a good request")
+//       }
+      
+//       response.end()
+//     }
+//   })
+// })
+
+// server.listen(port)
+
+const express = require('express')
 const fs = require('fs')
-const url = require('url')
-
+const app = express()
 const port = 80
-// var data = fs.readFileSync("DataFile.JSON", "utf8")
 
-const server = http.createServer(function (request, response) {
-  fs.readFile("DataFile.JSON", function(err, data){
+function IsJSON(data)
+{
+  try
+  {
+    JSON.parse(data)
+  }
+  catch (e)
+  {
+    console.log(e)
+    return false
+  }
+  return true
+}
+
+app.get('/api/cars', (req, res) => {
+  fs.readFile("DataFile.json", function(err, data){
     if (err)
     {
-      console.log("Something went wrong...")
+      res.write("Something went wrong...")
       console.log(err)
     }
     else
     {
-      if (request.url == "/cars")
-      {
-        // response.writeHead(200, {"Content-Type": "text/html"})
-        response.write(data)
-      }
-      else
-      {
-        response.write("Not a good request")
-      }
-      
-      response.end()
+        if (IsJSON(data))
+        {
+          res.write(data)
+        }
+        else
+        {
+          res.write("Sorry, something need to be fixed")
+          console.log("Not JSON")
+        }
     }
+    res.end()
   })
 })
 
-server.listen(port)
+app.get('*', function(req, res){
+  res.status(404).send('Looks like you have lost the path ;)');
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
