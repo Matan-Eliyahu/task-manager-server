acfb1461745879d452aa8de2b9adc03fed2b9d23
Matan <email>
2021-07-22 17:59:23 +0300

    Created CRUD for tasks (Working with json file for containing the tasks)

--- Server.js
+++ Server.js
@@ -1,40 +1,10 @@
-// const http = require('http')
-// const fs = require('fs')
-// const url = require('url')
-
-// const port = 80
-// // var data = fs.readFileSync("DataFile.JSON", "utf8")
-
-// const server = http.createServer(function (request, response) {
-//   fs.readFile("DataFile.JSON", function(err, data){
-//     if (err)
-//     {
-//       console.log("Something went wrong...")
-//       console.log(err)
-//     }
-//     else
-//     {
-//       if (request.url == "/cars")
-//       {
-//         // response.writeHead(200, {"Content-Type": "text/html"})
-//         response.write(data)
-//       }
-//       else
-//       {
-//         response.write("Not a good request")
-//       }
-      
-//       response.end()
-//     }
-//   })
-// })
-
-// server.listen(port)
-
 const express = require('express')
 const fs = require('fs')
+
 const app = express()
 const port = 80
+
+var new_task_id = 0
 
 function IsJSON(data)
 {
@@ -50,7 +20,40 @@ function IsJSON(data)
   return true
 }
 
+function GetFullTime(){
+  var date_obj = new Date()
+  var day = date_obj.getDate()
+  var month = date_obj.getMonth() + 1
+  var year = date_obj.getFullYear()
+  var time = date_obj.getHours() + ":" + date_obj.getMinutes() + ":" + date_obj.getSeconds()
+
+  return day + "-" + month + "-" + year + ", " + time
+}
+
+app.get('/api/tasks/:id', function(req, res){
+  var data = fs.readFileSync("TasksFile.JSON")
+  var JSON_obj_file = JSON.parse(data)
+
+  var task_id = req.params.id
+
+  res.send(JSON_obj_file[task_id])
+})
+
+app.get('/api/tasks', function(req, res){
+  var data = fs.readFileSync("TasksFile.JSON")
+  var JSON_obj_file = JSON.parse(data)
+
+  var tasks_Array = []
+
+  for (const task in JSON_obj_file) {
+    tasks_Array.push(JSON_obj_file[task])
+  }
+  
+  res.send(tasks_Array)
+})
+
 app.get('/api/cars', (req, res) => {
+  console.log(req.color)
   fs.readFile("DataFile.json", function(err, data){
     if (err)
     {
@@ -75,6 +78,93 @@ app.get('/api/cars', (req, res) => {
 
 app.get('*', function(req, res){
   res.status(404).send('Looks like you have lost the path ;)');
+})
+
+app.use(express.json())
+
+app.post('/api/task', function(req, res){
+  var data = fs.readFileSync("TasksFile.JSON")
+  var JSON_obj_file = JSON.parse(data)
+  
+  var JSON_obj_task = req.body
+
+  JSON_obj_task.createTime = GetFullTime()
+
+  //JSON_obj_task.id = new_task_id
+
+  JSON_obj_file[new_task_id] = JSON_obj_task
+  new_task_id += 1
+
+  fs.writeFile("TasksFile.JSON", JSON.stringify(JSON_obj_file)+"\n", function(err){
+    if(err)
+    {
+      console.log(err)
+      console.log("Failed to write in the file")
+    }
+    else
+    {
+      //res.sendStatus(201)
+      res.send(JSON.stringify(new_task_id - 1))
+      console.log("Created")
+      // res.end()
+    }
+  })
+})
+
+app.put('/api/tasks/:id', function(req, res){
+  var data = fs.readFileSync("TasksFile.JSON")
+  var JSON_obj_file = JSON.parse(data)
+
+  var task_id = req.params.id
+  var JSON_obj_fields = req.body
+
+  for(var field in JSON_obj_fields)
+  {
+    JSON_obj_file[task_id][field] = JSON_obj_fields[field]
+  }
+
+  JSON_obj_file[task_id]["lastUpdatedTime"] = GetFullTime()
+  
+  fs.writeFile("TasksFile.JSON", JSON.stringify(JSON_obj_file)+"\n", function(err){
+    if(err)
+    {
+      console.log(err)
+      console.log("Failed to write in the file")
+      res.send(false)
+    }
+    else
+    {
+      //res.sendStatus(200)
+      console.log("Updated")
+      res.send(true)
+    }
+    // res.end()
+  })
+})
+
+app.delete('/api/tasks/:id', function(req, res){
+  var data = fs.readFileSync("TasksFile.JSON")
+  var JSON_obj_file = JSON.parse(data)
+
+  var task_id = req.params.id
+  
+  delete JSON_obj_file[task_id]
+  
+  fs.writeFile("TasksFile.JSON", JSON.stringify(JSON_obj_file)+"\n", function(err){
+    if(err)
+    {
+      console.log(err)
+      console.log("Failed to write in the file")
+      res.send(false)
+    }
+    else
+    {
+      //res.sendStatus(301)
+      console.log("Deleted")
+      res.send(true)
+    }
+    // res.end()
+  })
 })
 
 app.listen(port, () => {
