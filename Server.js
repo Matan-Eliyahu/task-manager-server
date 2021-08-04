39a5746556937f67a3425e29296fd1e6f14a6f9a
Matan <email>
2021-08-04 04:01:45 +0300

    Deleted IsJSON function

--- Server.js
+++ Server.js
@@ -1,28 +1,11 @@
 const express = require('express')
-const fs = require('fs')
 const db = require('./db.js')
 
 const app = express()
 app.use(express.json())
-
-var new_task_id = 0
-const port = 80
-
 require('./Routs/tasksRouts.js')(app, db)
 
-function IsJSON(data)
-{
-  try
-  {
-    JSON.parse(data)
-  }
-  catch (e)
-  {
-    console.log(e)
-    return false
-  }
-  return true
-}
+const port = 80
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
